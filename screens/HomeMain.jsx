2345e3a937e6ba1615376d0585e41b3113606033
Alex Nastase <74926940+AlexandruStefan15@users.noreply.github.com>
2025-04-12 11:22:22 +0300

    good

--- screens/HomeMain.jsx
+++ screens/HomeMain.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import Header from "@/components/Header";
 import { Colors } from "@/constants";
+import { ThemeContext } from "@react-navigation/native";
 
 export default function HomeMain({ navigation }) {
+	const { theme } = React.useContext(ThemeContext);
+
 	return (
 		<SafeAreaView style={styles.screen}>
 			<ScrollView>
