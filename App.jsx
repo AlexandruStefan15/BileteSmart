5023416ced2554490b5f0bf233517c21fcd2ddbc
Alex Nastase <74926940+AlexandruStefan15@users.noreply.github.com>
2025-04-12 11:21:10 +0300

    good

--- App.jsx
+++ App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import RootNavigator from "./navigation/RootNavigator";
 import { useTheme } from "./hooks/useTheme";
+import { ThemeContext } from "./Context/ThemeContext";
 
 function App() {
 	const { theme, setTheme } = useTheme();
 
-	return <RootNavigator />;
+	return (
+		<ThemeContext.Provider value={{ theme, setTheme }}>
+			<RootNavigator />
+		</ThemeContext.Provider>
+	);
 }
 
 export default App;
