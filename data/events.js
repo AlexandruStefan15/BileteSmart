18bae8ed50d420144be87667c7f3984e9074252c
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2025-04-25 13:46:28 +0300

    good

---
+++ data/events.js
@@ -0,0 +1,31 @@
+import { images } from "@/assets/images";
+
+export const eventsByLocation = {
+	1: [
+		{
+			id_event: "129",
+			title: "CS GLORIA 2018 BN-CSM SLATINA",
+			event_img: "https://biletesmart.ro/poster/82371745484927864603.05 GLORIA SLATINA.jpg",
+			event_type: "Sportiv",
+			event_type_name: "Liga Florilor",
+			date: "2025-05-03 17:00:00",
+			demo: "1",
+			logo_images: [],
+		},
+	],
+
+	2: [
+		{
+			id_event: "123",
+			title: "FC ARGES - CSM GALATI",
+			event_img: "https://biletesmart.ro/poster/418617450564854125fc arges - csm galati v.jpg",
+			event_type: "Sportiv",
+			event_type_name: "Baschet Masculin SC 2024-2025",
+			date: "2025-04-26 18:30:00",
+			demo: "0",
+		},
+	],
+
+	3: [],
+	4: [],
+};
