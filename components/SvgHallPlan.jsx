0ec119369af494dc3252bbf2c6ff8c6ec70616c6
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2025-05-01 18:00:01 +0300

    good

--- components/SvgHallPlan.jsx
+++ components/SvgHallPlan.jsx
@@ -7,6 +7,9 @@ import { roomsWithSeatsByLocation } from "@/data/hallPlans";
 
 const SvgHallPlan = ({ locationId = 1 }) => {
 	const location = roomsWithSeatsByLocation[locationId];
+	const field_path = location.field_path_d;
+	const rooms = location.rooms;
+
 	if (!location) {
 		return null;
 	}
@@ -14,9 +17,12 @@ const SvgHallPlan = ({ locationId = 1 }) => {
 	console.log(location.field_path_d);
 
 	return (
-		<View>
+		<View style={{ justifyContent: "center", alignItems: "center", paddingBlock: 50 }}>
 			<Svg style={{ margin: "auto" }} width={355} height={355} fill="none" viewBox="0 0 775 851">
-				<Path d={location.field_path_d} stroke="black" strokeWidth={3} strokeMiterlimit={10} />
+				<Path d={field_path} stroke="black" strokeWidth={3} strokeMiterlimit={10} />
+				{rooms.map((room) => (
+					<Path key={room.id} d={room.path_d} fill="#BFBFBF" />
+				))}
 			</Svg>
 		</View>
 	);
