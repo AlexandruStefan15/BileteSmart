9b85e4206e58de69bd4885c3389300b37ba5fb43
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2025-08-07 20:37:31 +0300

    good

--- screens/EventDetailsScreen.jsx
+++ screens/EventDetailsScreen.jsx
@@ -66,7 +66,7 @@ const EventDetailsScreen = ({ navigation, route }) => {
 							rooms={rooms}
 							field_path={locationFieldPath}
 							read_only={true}
-							height={260}
+							height={SCREEN_HEIGHT * 0.35} //old value:260
 						/>
 						<View>
 							{rooms.find((room) => room.free_seats > 0) ? (
