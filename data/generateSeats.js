3af55fe7387d94e431752cff13c41f43d83f1d57
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2025-08-17 01:26:09 +0300

    good

--- data/generateSeats.js
+++ data/generateSeats.js
@@ -7,14 +7,14 @@ function extractPathsFromSVG(svgString) {
 	const paths = dom.window.document.querySelectorAll("path");
 
 	const seatsArray = Array.from(paths).map((path, index) => ({
-		id_seat: (30857 + index).toString(),
-		id_room: 3,
+		id_seat: (550857 + index).toString(),
+		id_room: 17,
 		path_d: path.getAttribute("d"),
 		price: 25,
 		row_no: "0",
 		col_no: "0",
 		seat_no: (index + 1).toString(),
-		room_name: "A3",
+		room_name: "B5",
 		is_valid: "1",
 		is_booking_blocked: "0",
 		is_for_child: "0",
